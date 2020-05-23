cf96a772e8d1eff9776ff9ab4090d560130314b2
gitName <a@b.c>
2020-05-23 15:31:34 +0530

    test msg

--- auto_git.py
+++ auto_git.py
@@ -2,25 +2,51 @@ import os
 import emoji
 
 
+repo_name = "ashr_with_coffee"
+commit_msg = "testing commit msg"
+
+
 def current_dir():
     return os.getcwd()
 
 
 # print(current_dir())
 
-# print("{{{}}}".format(42))  # {42}
-
 
 def creating_md():
     print("Creating README File for you!")
-    print("Please Wait...")
-    # print("\N{grinning face}")
-    print(emoji.emojize(":zipper-mouth_face:"))
+    print(emoji.emojize("\nPlease Wait...:slightly_smiling_face:"))
+    os.system("echo '# {}'>>README.md".format(repo_name))
+    print(
+        emoji.emojize(
+            "\nCreated README.md successfully for you!:smiling_face_with_halo:"
+        )
+    )
 
 
 def git_init():
-    print("Initializing the repository...")
+    print(
+        emoji.emojize(
+            "\nInitializing your repository...:smiling_face_with_smiling_eyes:"
+        )
+    )
     os.system("git init")
+
+
+def git_add():
+    print(emoji.emojize("\n Initializing Git Add Command for you...:smiling_face:"))
+    os.system("git add .")
+
+
+def git_status():
+    print(emoji.emojize("Here's the status of this REPO...:face_savoring_food:"))
+    os.system("git status")
+
+
+def git_commit():
+    print(emoji.emojize("Ready To Commit Your MESS...?:expressionless_face:"))
+    # os.system("git commit -m {}".format(msg))
+    os.system("git commit")
 
 
 def my_git_cmd():
@@ -31,3 +57,6 @@ if __name__ == "__main__":
     current_dir()
     creating_md()
     git_init()
+    git_add()
+    git_status()
+    git_commit()
