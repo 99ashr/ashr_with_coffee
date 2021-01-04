b724c35b561bba19225d744ff8b1e9f3407241ff
Ash <gaurashish99@gmail.com>
2021-01-04 17:47:15 +0530

    voice_added_to_git

--- automated_git/auto_git.py
+++ automated_git/auto_git.py
@@ -1,12 +1,31 @@
 import os
 import emoji
 import pickle
+import pyttsx3
+
+
+# &Create Object and Set voice
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')  # getting details of current voice
+# changing index, changes voices. o for male
+# engine.setProperty('voice', voices[0].id)
+# changing index, changes voices. 1 for female
+engine.setProperty('voice', voices[1].id)
 
 
 # repo_name = "ashr_with_coffee"
 all_repos = []
+
+
+def talk(talk):
+    engine.say(talk)
+    engine.runAndWait()
+
+
+talk("Please enter the name of your repository")
 repo_name = input(
-    emoji.emojize("Enter the name of your repository here!:pensive_face:\nHere!:\t")
+    emoji.emojize(
+        "Enter the name of your repository here!:pensive_face:\nHere!:\t")
 )
 
 
@@ -45,6 +64,7 @@ def creating_md():
 
 
 def git_init():
+    talk("Initializing your repository")
     print(
         emoji.emojize(
             "\nInitializing your repository...:smiling_face_with_smiling_eyes:"
@@ -54,19 +74,24 @@ def git_init():
 
 
 def git_add():
+    talk("Adding files to commit")
     print(emoji.emojize("\n Initializing Git Add Command for you...:smiling_face:"))
     os.system("git add .")
 
 
 def git_status():
+    talk("These files are added")
     print(emoji.emojize("Here's the status of this REPO...:face_savoring_food:"))
     os.system("git status")
 
 
 def git_commit():
+    talk("Ready To Commit")
     print(emoji.emojize("Ready To Commit Your MESS...?:expressionless_face:"))
+    talk("Please enter your commit message")
     commit_msg = input(
-        emoji.emojize("Please enter your commit message here:drooling_face:\n message:")
+        emoji.emojize(
+            "Please enter your commit message here:drooling_face:\n message:")
     )
     commit = "git commit -m {}".format(commit_msg)
     os.system(commit)
@@ -78,11 +103,13 @@ remote = "git remote add origin"
 
 def connect_remote():
     # repo_name = readfun()
+    talk("Connecting to your remote directory")
     print(emoji.emojize("Connecting to your remote directory...:lying_face:"))
     os.system("{} {}{}.git".format(remote, user_repo_link, repo_name))
 
 
 def git_push():
+    talk("We're good to go.")
     os.system("git push")
 
 
@@ -92,6 +119,7 @@ if __name__ == "__main__":
         try:
             repo_name = readfun()
         except FileNotFoundError:
+            talk("oops something is wrong")
             print("please enter the name manually!")
     else:
         writefun()
@@ -103,3 +131,4 @@ if __name__ == "__main__":
     git_status()
     connect_remote()
     git_push()
+    engine.stop()
